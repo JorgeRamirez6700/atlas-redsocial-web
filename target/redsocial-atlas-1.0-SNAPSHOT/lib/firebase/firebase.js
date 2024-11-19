5e290d70a8b06472a56a20f162a4660a17486042
Jorge Luis Ramirez Echeverría <109499407+JorgeRamirez6700@users.noreply.github.com>
2024-11-19 11:39:03 -0700

    Testeo de firebase, keys viejas removidas.

--- target/redsocial-atlas-1.0-SNAPSHOT/lib/firebase/firebase.js
+++ target/redsocial-atlas-1.0-SNAPSHOT/lib/firebase/firebase.js
@@ -8,11 +8,11 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: 'AIzaSyBUx0b2B1ienANFiPULiugJVR3KDhZvHXg',
-  authDomain: 'marchantes-00.firebaseapp.com',
-  projectId: 'marchantes-00',
-  storageBucket: 'marchantes-00.appspot.com',
-  messagingSenderId: '951446658923',
-  appId: '1:951446658923:web:65b9d531970573aa9eb62e',
-  measurementId: 'G-TV9JQMYT3H',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
